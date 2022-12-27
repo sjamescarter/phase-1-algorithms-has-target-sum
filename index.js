acfcde89bf10be8db9b64af0cd7fbff35284294e
sjamescarter <stephenjamescarter@gmail.com>
2022-12-27 15:59:15 -0600

    commit tree be8075086019141e4d6acb4400803b576b300a97

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for(let i = 0; i < array.length; i++){
+    for(let j = i + 1; j < array.length; j++){
+      if (array[i] + array[j] === target) {
+        return true
+      }
+    }
+  } return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n**2)
 */
 
 /* 
   Add your pseudocode here
+  Create two loops
+  Add numbers from each loop
+  Compare to target number
+  Return true if target exists
+  Return false if target does not exist
 */
 
 /*
   Add written explanation of your solution here
+  This code uses nested loops to iterate over an array. Adding the two iterations
+  allows the code to compare the sum to the target and return true or false if the 
+  target exists within the array.
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +44,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 2, 5, 10, 15], 35));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([22, 15, 7, 4, 3, 0], 4));
 }
 
 module.exports = hasTargetSum;
